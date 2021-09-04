0387f5cfe52c1e5b40eec5bf612b4d8380db352a
Antoine Tsinga <antoine.tsinga@orthoin3d.com>
2021-09-04 21:17:29 +0200

    route vers les pages homme et femme

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
 
   <header>
     <div id="menu">
-      <router-link to="" id="man"> MAN </router-link>
+      <router-link to="catalogue/Hommes" id="man"> MAN </router-link>
       <span class="separateur_verticale"></span>
-      <router-link to="" id="woman"> WOMAN </router-link>
+      <router-link to="catalogue/Femmes" id="woman"> WOMAN </router-link>
     </div>
   </header>
 
